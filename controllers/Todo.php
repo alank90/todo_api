7f9756ccf0ffd935be79349e1c14c2eec3051783
Alan K <akillian@outlook.com>
2016-11-14 15:16:11 -0500

    Initial Commit

---
+++ controllers/Todo.php
@@ -0,0 +1,50 @@
+<?php
+
+class Todo
+{
+    private $_params;
+    // A __construct is executed when the class is instantiated 
+    public function __construct($params)
+    {
+        $this->_params = $params;
+		//print_r($this->_params);
+    }
+     
+    public function createAction()
+{
+    //create a new todo item
+    $todo = new TodoItem();
+    $todo->title = $this->_params['title'];
+    $todo->description = $this->_params['description'];
+    $todo->due_date = $this->_params['due_date'];
+    $todo->is_done = 'false';
+     
+    //pass the user's username and password to authenticate the user
+    $todo->save($this->_params['username'], $this->_params['userpass']);
+     
+    //return the todo item in array format
+    return $todo->toArray();
+}
+     
+   /* public function readAction()
+    {
+        //read all the todo items
+        
+      //pass the user's username and password to authenticate the user
+    $todo->read($this->_params['username'], $this->_params['userpass']);
+     
+    //return the todo item in array format
+    return $todo->toArray();
+        
+    }
+  */   
+    public function updateAction()
+    {
+        //update a todo item
+    }
+     
+    public function deleteAction()
+    {
+        //delete a todo item
+    }
+}
---
+++ models/TodoItem.php
@@ -0,0 +1,83 @@
+<?php
+
+    class TodoItem
+{
+    public $todo_id;
+    public $title;
+    public $description;
+    public $due_date;
+    public $is_done;
+     
+    public function save($username, $userpass)
+    {
+        //get the username/password hash
+        $userhash = sha1("{$username}_{$userpass}");
+        if( is_dir(DATA_PATH . "/{$userhash}") === false ) {
+            mkdir(DATA_PATH . "/{$userhash}");
+        }
+         
+        //if the $todo_id isn't set yet, it means we need to create a new todo item
+        if( is_null($this->todo_id) || !is_numeric($this->todo_id) ) {
+            //the todo id is the current time
+            $this->todo_id = time();
+        }
+         
+        //get the array version of this todo item
+        $todo_item_array = $this->toArray();
+      
+        //save the serialized array version into a file
+        $success = file_put_contents(DATA_PATH . "/{$userhash}/{$this->todo_id}.txt", serialize($todo_item_array));
+         
+        //if saving was not successful, throw an exception
+        if( $success === false ) {
+            throw new Exception('Failed to save todo item');
+        }
+         
+        //return the array version
+        return $todo_item_array;
+    }
+
+
+/*===========  New Read function ==================
+  public function read($username, $userpass)
+    {
+    	echo 'Im in read Function!';
+        //get the username/password hash
+        $userhash = sha1("{$username}_{$userpass}");
+		 if( is_dir(DATA_PATH . "/{$userhash}") === false ) {
+            echo 'Error. No Such User file. ';
+        }
+         
+        //if the $todo_id doesn't exist, throw an error
+        if( is_null($this->todo_id) || !is_numeric($this->todo_id) ) {
+              echo 'Error. No matching todo_id ';
+        }
+         
+        //get the array version of this todo item
+        $todo_item_array = $this->toArray();
+      
+        //read the serialized array version into a variable
+        $success = file_get_contents(DATA_PATH . "/{$userhash}/{$this->todo_id}.txt");
+         
+        //if saving was not successful, throw an exception
+        if( $success === false ) {
+            throw new Exception('Failed to read todo item');
+        }
+         
+        //return the array version
+        return $todo_item_array;
+    }
+   */  
+    public function toArray()
+    {
+        //return an array version of the todo item
+        return array(
+            'todo_id' => $this->todo_id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'due_date' => $this->due_date,
+            'is_done' => $this->is_done
+        );
+    }
+}
+?>
